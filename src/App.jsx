507fcec4aa51b1ed6c5e8d691ceadbfa9fd20774
malcomcrab <malcomcrabesq@outlook.com>
2025-05-21 14:53:46 +0100

    Build: Install axios and successfully fetch 'meals' and 'people' data from the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 function App() {
-  const [count, setCount] = useState(0)
+  const [people, setPeople] = useState([])
+  const [meals, setMeals] = useState([])
+  
+   function getUsers() {
+      axios.get('http://localhost:3000/people',
+        { method: "cors" },
+        { withCredentials: true },
+      )
+      .then(function (response) {
+        console.log(response);
+        setPeople(response.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+
+  function getMeals() {
+    axios.get('http://localhost:3000/meals',
+      { method: "cors" },
+      { withCredentials: true },
+    )
+    .then(function (response) {
+      console.log(response);
+      setMeals(response.data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+}
+
+
+
+
+  useEffect(() => {
+    getUsers()
+    getMeals()
+  }, [])
 
   return (
     <>
