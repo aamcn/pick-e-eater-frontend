71ad5e7ed7ead7d8bd607203207379c01ed87275
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-19 13:34:09 +0100

    Test: Add test suite and testing for MealResultsDisplay

--- src/components/addMeal/AddMealForm/addMealForm.test.jsx
+++ src/components/addMeal/AddMealForm/addMealForm.test.jsx
@@ -5,7 +5,7 @@ import { describe, it, expect, vi} from "vitest";
 const mockProps = {
     toggleFormDisplay: vi.fn(),
     getMeals: vi.fn(),
-    allMeals: []
+    allMeals: [ { id: 1, name: "Pizza", difficulty: 300 }, { id: 2, name: "Burger", difficulty: 500 } ]
 }
 
 describe("AddMealForm", () => {
@@ -14,4 +14,5 @@ describe("AddMealForm", () => {
     const formElement = screen.getByTestId("add-meal-form");
     expect(formElement).toBeInTheDocument();
   });
+
 }); 
