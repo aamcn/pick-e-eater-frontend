3afc9d72129340e534b160493c795a9c4d398926
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-21 11:23:04 +0100

    Refactor: component returns a seperate element if no meals are returned.

--- src/components/randomMealSelector/randomMealsDisplay/RandomMealsDisplay.jsx
+++ src/components/randomMealSelector/randomMealsDisplay/RandomMealsDisplay.jsx
@@ -1,12 +1,19 @@
 import "./randomMealsDisplay.scss";
 
 function RandomMealsDisplay({ randomMeals }) {
-  
+
+
+  if (!randomMeals || randomMeals.length === 0) {
+    return <div className="randomDisplayContainer" data-testid="random-meal-backdrop">
+      <p className="randomMealName" data-testid="no-meals-element">No meals found</p>
+    </div>;
+  }
+
   //Displays each meal from the array passed in as props
   return (
-    <div className="randomDisplayContainer">
+    <div className="randomDisplayContainer" data-testid="random-meal-backdrop">
       {randomMeals.map((meal) => {
-        return <p className="randomMealName">{meal}</p>;
+        return <p className="randomMealName" key={meal.id} data-testid="random-meal-item">{meal.name}</p>;
       })}
     </div>
   );
