a31124618673801cb796c5aed3b9046a0046c3a9
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-29 09:59:55 +0100

    Test: Add test suite and tests for 'filterArrayDuplicates

---
+++ src/modules/filterArrayDuplicates/filterArrayDuplicates.js
@@ -0,0 +1,8 @@
+
+// Push entry to array if it doesn't already exist in array.
+export  function filterArrayDuplicates(array, entry) {
+  if(!entry){
+    throw new Error("No entry provided to filterArrayDuplicates function");
+  }
+    !array.includes(entry) ? array.push(entry) : null;
+  }
