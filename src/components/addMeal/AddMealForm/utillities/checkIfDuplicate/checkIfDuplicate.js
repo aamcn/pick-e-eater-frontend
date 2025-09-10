22dccda17f7b07672d9cadd06689f46631788e5d
aaronmcnulty <malcomcrabesq@outlook.com>
2025-09-10 17:10:29 +0100

    Chore: Update function nation for AddMealForm utility functions

--- src/components/addMeal/AddMealForm/utillities/checkIfDuplicate/checkIfDuplicate.js
+++ src/components/addMeal/AddMealForm/utillities/checkIfDuplicate/checkIfDuplicate.js
@@ -1,11 +1,17 @@
-
-
-export function checkIfDuplicate(allMealNames, setErrorMessage, setIsMealDuplicate, inputMealName){
-    if(allMealNames.includes(inputMealName.toLowerCase())){
-      setErrorMessage(`${inputMealName} already exists`)
-      setIsMealDuplicate(true)
-    } else {
-      setErrorMessage(null)
-      setIsMealDuplicate(false)
-    }
+// Function to check if the input meal name is a duplicate.
+export function checkIfDuplicate(
+  allMealNames,
+  setErrorMessage,
+  setIsMealDuplicate,
+  inputMealName,
+) {
+  // Check if the inputMealName exists in allMealNames array, if true update errorMessage and isMealDuplicate.
+  if (allMealNames.includes(inputMealName.toLowerCase())) {
+    setErrorMessage(`${inputMealName} already exists`);
+    setIsMealDuplicate(true);
+  } else {
+    // If not a duplicate, clear any previous error messages and set duplicate flag to false.
+    setErrorMessage(null);
+    setIsMealDuplicate(false);
   }
+}
--- src/components/addMeal/AddMealForm/utillities/handleMealNameChange/handleMealNameChange.js
+++ src/components/addMeal/AddMealForm/utillities/handleMealNameChange/handleMealNameChange.js
@@ -1,5 +1,5 @@
-
-export function handleMealNameChange(event, setInputMealName){
-    setInputMealName(event.target.value);
-    console.log(event.target.value);
-  };
+// Function to handle changes to the meal name input field.
+export function handleMealNameChange(event, setInputMealName) {
+  setInputMealName(event.target.value);
+  console.log(event.target.value);
+}
