49b2e77ad9f1bc8b8e244252764795c2e85e3874
aaronmcnulty <malcomcrabesq@outlook.com>
2025-09-10 17:17:36 +0100

    Chore: Clarify component notation. Tidy and format component and linked files

--- src/components/filterByDiner/checkBoxTemplate/CheckBoxTemplate.jsx
+++ src/components/filterByDiner/checkBoxTemplate/CheckBoxTemplate.jsx
@@ -4,16 +4,21 @@ import { tickedBox } from "./utilities/tickedBox/tickedBox";
 
 function CheckBoxTemplate({ diner, setSelectedDiners, selectedDiners }) {
   const [isClicked, setIsClicked] = useState(false);
-  
+
   /*
-    Adds or removes a diner depending on whether the checkbox is checked or unchecked.
+    Adds or removes a diner depending on the state of the 'isClicked' boolean.
     If checkbox is checked, insert the defaultValue (A diner) into the selectedDiners array and updates state.
-    If checkbox is unchecked, filter the defaultValue (diner) from the selectedDiners array and updates state.
+    If checkbox is NOT checked, filter the defaultValue (diner) from the selectedDiners array and updates state.
   */
-  
 
   const handleClick = () => {
-    tickedBox(selectedDiners, setIsClicked, isClicked, diner, setSelectedDiners);
+    tickedBox(
+      selectedDiners,
+      setIsClicked,
+      isClicked,
+      diner,
+      setSelectedDiners,
+    );
   };
 
   return (
--- src/components/filterByDiner/checkBoxTemplate/utilities/tickedBox/tickedBox.js
+++ src/components/filterByDiner/checkBoxTemplate/utilities/tickedBox/tickedBox.js
@@ -1,14 +1,19 @@
-
 // Ticked box functionality, adds or removes a diner from the selectedDiners list depending on isClicked status.
-export function tickedBox(selectedDiners, setIsClicked, isClicked, diner, setSelectedDiners) {
-    if (!isClicked)
-      // Add diner to selectedDiners
-      setSelectedDiners(selectedDiners => [...selectedDiners, diner]);
-    setIsClicked(true);
-    if (isClicked) {
-      // Remove diner from selectedDiners
-      const filteredDiners = selectedDiners.filter((value) => value != diner);
-      setSelectedDiners(filteredDiners);
-      setIsClicked(false);
-    }
-  };
+export function tickedBox(
+  selectedDiners,
+  setIsClicked,
+  isClicked,
+  diner,
+  setSelectedDiners,
+) {
+  if (!isClicked)
+    // Add diner to selectedDiners
+    setSelectedDiners((selectedDiners) => [...selectedDiners, diner]);
+  setIsClicked(true);
+  if (isClicked) {
+    // Remove diner from selectedDiners
+    const filteredDiners = selectedDiners.filter((value) => value != diner);
+    setSelectedDiners(filteredDiners);
+    setIsClicked(false);
+  }
+}
