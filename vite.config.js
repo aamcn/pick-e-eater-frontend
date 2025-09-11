a0257b098f6a6bf0f998e3316bfe449f46400316
aaronmcnulty <malcomcrabesq@outlook.com>
2025-09-11 16:21:05 +0100

    Update vite config to include base.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "pick-e-eater-frontend/",
   test: {
     globals: true,
     environment: "jsdom",
