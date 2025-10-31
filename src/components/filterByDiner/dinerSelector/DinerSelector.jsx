d9829650633b79fd1bfd40aaad986dedf285c7f9
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-31 11:23:49 +0000

    Adjust DinerSelector titlecolor, style and placement

--- src/components/filterByDiner/dinerSelector/DinerSelector.jsx
+++ src/components/filterByDiner/dinerSelector/DinerSelector.jsx
@@ -12,13 +12,15 @@ function DinerSelector({
     Calls the function to collect diners dislikes everytime 'selectedDiners' is updated.
   */
   useEffect(() => {
-   setDislikedMeals(collectDinerDislikes(selectedDiners));
+    setDislikedMeals(collectDinerDislikes(selectedDiners));
   }, [selectedDiners, setDislikedMeals]);
-
 
   return (
     <div className="diner-selector-container" data-testid="diner-selector">
-      <div className="diner-selector-title-container" data-testid="diner-selector-title">
+      <div
+        className="diner-selector-title-container"
+        data-testid="diner-selector-title"
+      >
         <h3 className="diner-selector-title">Choose Who's Eating</h3>
       </div>
       <div className="diner-check-boxes">
